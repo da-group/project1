59d4e2b5f0c7517e6903fc7ff9b6b6bd5eb97ba8
milliondegree <zhangjiachi1007@gmail.com>
2018-10-03 14:21:04 -0400

    food

--- food.py
+++ food.py
@@ -7,11 +7,13 @@
 import json
 
 
-with open('./dataset/food-enforcement-0001-of-0001.json') as f:
-    d = json.load(f)
-    print(type(d))
-    print(d.keys())
-    print(type(d['results']))
-    print(len(d['results']))
-    print(d['results'][0])
+if __name__ == '__main__':
 
+    f1 = open('./dataset/food-enforcement-0001-of-0001.json')
+    f2 = open('./dataset/food-event-0001-of-0001.json')
+    d1 = json.load(f1)
+    d2 = json.load(f2)
+    print(len(d1['results']))
+    print(len(d2['results']))
+    print(d1['results'][0])
+    print(d2['results'][0])
