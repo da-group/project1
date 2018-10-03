142dc94ab970c9e22f59766aeab89c0823126ef9
milliondegree <zhangjiachi1007@gmail.com>
2018-10-03 14:04:18 -0400

    food test

---
+++ food.py
@@ -0,0 +1,11 @@
+import json
+
+
+with open('./dataset/food-enforcement-0001-of-0001.json') as f:
+    d = json.load(f)
+    print(type(d))
+    print(d.keys())
+    print(type(d['results']))
+    print(len(d['results']))
+    print(d['results'][0])
+
