c0b8441e06aacfa1eabbceef97b5e9897d35ca66
milliondegree <zhangjiachi1007@gmail.com>
2018-10-05 00:49:34 -0400

    crime

--- CrimeDC.py
+++ CrimeDC.py
@@ -1,12 +1,19 @@
+##############################################################
+# Author: DA-GROUP
+# data source: http://opendata.dc.gov/datasets/crime-incidents-in-2017?page=9
+##############################################################
+
+
+import csv
 import requests
-import json
 
 
 if __name__ == '__main__':
-    re = requests.get('https://opendata.arcgis.com/datasets/6af5cb8dc38e4bcbac8168b27ee104aa_38.geojson').json()
-    print(type(re))
-    print(re.keys())
-    print(len(re['features']))
-    for i in range(10):
-        print(re['features'][i])
+    with open('./dataset/crime2017.csv', 'w') as f:
+        writer = csv.writer(f)
+        response = requests.get('https://opendata.arcgis.com/datasets/6af5cb8dc38e4bcbac8168b27ee104aa_38.geojson').json()
+        data = response['features']
+        writer.writerow(data[0]['properties'].keys())
+        for d in data:
+            writer.writerow([d['properties'][k] for k in d['properties'].keys()])
 
--- food.py
+++ food.py
@@ -11,9 +11,17 @@ if __name__ == '__main__':
 
     f1 = open('./dataset/food-enforcement-0001-of-0001.json')
     f2 = open('./dataset/food-event-0001-of-0001.json')
+    f3 = open('./dataset/drug-event-0002-of-0004.json')
     d1 = json.load(f1)
     d2 = json.load(f2)
+    d3 = json.load(f3)
+    '''
     print(len(d1['results']))
     print(len(d2['results']))
+    print(len(d3['results']))
     print(d1['results'][0])
     print(d2['results'][0])
+    print(d3['results'][0])
+    '''
+    for i in range(5):
+        print(d2['results'][i])
