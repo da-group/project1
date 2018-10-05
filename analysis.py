06b37b9ee26bb922b298663d8d331e99dd92a285
milliondegree <zhangjiachi1007@gmail.com>
2018-10-05 01:16:23 -0400

    an

--- analysis.py
+++ analysis.py
@@ -9,4 +9,7 @@ import pandas as pd
 if __name__ == '__main__':
     myData = pd.read_csv('dataset/crime2017.csv', sep=',', encoding='latin1')
     print(myData.head())
+    index = myData.isnull().any(axis=1)
+    print(sum(index))
+    print(myData.shape)
 
