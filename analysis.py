8d8c9498a5623a4adf1ff74a605ea54957304a9d
milliondegree <zhangjiachi1007@gmail.com>
2018-10-05 01:10:50 -0400

    analyze

---
+++ analysis.py
@@ -0,0 +1,12 @@
+##########################################
+# Author: Jiachi Zhang
+# E-mail: zhangjiachi1007@gmail.com
+##########################################
+
+
+import pandas as pd
+
+if __name__ == '__main__':
+    myData = pd.read_csv('dataset/crime2017.csv', sep=',', encoding='latin1')
+    print(myData.head())
+
